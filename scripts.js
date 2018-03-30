7b6b94685471b02f1eff113d8ef5ac0ed3c5ad0f
Michael Lamb <mflambjr@gmail.com>
2018-03-29 21:38:06 -0400

    First commit

---
+++ scripts.js
@@ -0,0 +1,8 @@
+class Wheel {
+    constructor() {
+        var game = this;
+        game.wrapper = document.createElement('div');
+        document.body.appendChild(game.wrapper);
+        game.answers = 
+    }
+} 
